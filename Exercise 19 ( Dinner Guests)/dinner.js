de07e9dd7cb404ac6ddd329dffe9c81c207585ed
aamir <s.aamirali91@example.com>
2024-03-13 22:59:00 +0500

    message

---
+++ Exercise 19 ( Dinner Guests)/dinner.js
@@ -0,0 +1,3 @@
+"use strict";
+let guestList = ["Aamir", "Hamza", "Taha"];
+console.log(`I am inviting ${guestList.length} people to dinner.`);
---
+++ Exercise 19 ( Dinner Guests)/dinner.ts
@@ -0,0 +1,2 @@
+let guestList = [ "Aamir","Hamza", "Taha"];
+console.log(`I am inviting ${guestList.length} people to dinner.`);
