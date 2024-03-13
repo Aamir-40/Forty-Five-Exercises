e41a618afb6343eed25c46d38bd6eea2de534c33
aamir <s.aamirali91@example.com>
2024-03-13 23:06:39 +0500

    message

---
+++ Exercise 24 ( More Conditional Tests)/app.ts
@@ -0,0 +1,31 @@
+import { toASCII } from "punycode";
+
+// Equality with strings
+console.log("Testing equality with strings:");
+console.log("apple" == "apple"); 
+console.log("apple" != "apple"); 
+
+// Using the lower case function
+console.log("Testing with lower case:");
+console.log("Apple".toLowerCase() == "apple"); 
+console.log("mango".toLowerCase() == "mango");
+
+// Numerical tests
+console.log("Numerical tests:");
+console.log(100 > 70); 
+console.log(90 < 81); 
+
+
+// Tests using "and" and "or" operators
+console.log("Tests with 'and' and 'or':");
+console.log(true && false); // False
+console.log(true || false); // True
+
+// Test whether an item is in a array
+let fruits = ["apple", "banana", "cherry"];
+console.log("Is 'apple' in fruits?");
+console.log(fruits.includes("apple")); // True
+
+// Test whether an item is not in a array
+console.log("Is 'mango' not in fruits?");
+console.log(!fruits.includes("mango")); // True
