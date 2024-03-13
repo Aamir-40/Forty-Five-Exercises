baa01f4cdedf24e90e778da53fd12d9fd891f8c7
aamir <s.aamirali91@example.com>
2024-03-13 23:13:20 +0500

    message

---
+++ Exercise 29 ( Favorite Fruit)/app.js
@@ -0,0 +1,17 @@
+"use strict";
+let favorite_fruits = ["apples", "bananas", "cherries", "mangoes", "peaches",];
+if (favorite_fruits.includes("bananas")) {
+    console.log("You really like bananas!");
+}
+if (favorite_fruits.includes("apples")) {
+    console.log("You really like apples!");
+}
+if (favorite_fruits.includes("cherries")) {
+    console.log("You really like cherries!");
+}
+if (favorite_fruits.includes("mangoes")) {
+    console.log("you really like mangoes");
+}
+if (favorite_fruits.includes("peaches")) {
+    console.log("you realy like peaches");
+}
