907ddea79d375d21e1fc034c32dd4da9da4003a2
aamir <s.aamirali91@example.com>
2024-03-13 23:05:47 +0500

    message

---
+++ Exercise 23 ( Conditional Tests )/app.js
@@ -0,0 +1,34 @@
+"use strict";
+// Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+//01 tests evaluating true
+let car = 'Toyota';
+console.log("Is car == 'Toyota'? I predict True.");
+console.log(car == 'Toyota');
+//02 tests evaluating true
+console.log("Is car === 'Toyota'? I predict True.");
+console.log(car === 'Toyota');
+//03 tests evaluating true
+console.log("Is car >= 'Toyota'? I predict True.");
+console.log(car >= 'Toyota');
+//04 tests evaluating true
+console.log("Is car >= 'Toyota'? I predict True.");
+console.log(car >= 'Toyota');
+//05 tests evaluating true
+console.log("Is car? I predict True.");
+console.log(car);
+//5 tests evaluate to False.
+//01 tests evaluating false
+console.log("Is car != 'Toyota'? I predict False.");
+console.log(car != 'Toyota');
+//02 tests evaluating false
+console.log("Is car !== 'Toyota'? I predict False.");
+console.log(car !== 'Toyota');
+//03 tests evaluating false
+console.log("Is car < 'Toyota'? I predict False.");
+console.log(car < 'Toyota');
+//04 tests evaluating true
+console.log("Is car > 'Toyota'? I predict False.");
+console.log(car > 'Toyota');
+//05 tests evaluating true
+console.log("Is !car? I predict False.");
+console.log(!car);
