2c173bea57eca7e364a5ec71295c1c9c43177998
aamir <s.aamirali91@example.com>
2024-03-13 23:07:55 +0500

    message

---
+++ Exercise 25 (Alien Colors #1)/app.js
@@ -0,0 +1,10 @@
+"use strict";
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points!");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    console.log("no out put");
+    // No output because the condition is false
+}
