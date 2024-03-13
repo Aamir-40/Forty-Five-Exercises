86bd387ce44fa7d85d96b49b7948f8b4004ab9f7
aamir <s.aamirali91@example.com>
2024-03-13 23:04:02 +0500

    message

---
+++ Exercise 22 ( Intentional error)/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let friends = ["Aamir", "Irfan", "Danial"];
+//console.log(friends[3]);  Intentional error: Arrays are zero-indexed, so index 3 is out of bounds.
+//friends[2] = "Danial"; orrecting the error by accessing a valid index.
+console.log(friends[2]);
