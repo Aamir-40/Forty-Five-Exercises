aaeb9355dc92ce73803b735e0126877193347855
aamir <s.aamirali91@example.com>
2024-03-13 23:01:35 +0500

    message

---
+++ Exercise 20 ( storage in array)/jap.js
@@ -0,0 +1,4 @@
+"use strict";
+//Think of something you could store in an array.
+let mountains = ["Everest", " K2 ", "Anapurna", "Nangaparbat"];
+console.log(" mountains i want to climb:", mountains);
---
+++ Exercise 20 ( storage in array)/jap.ts
@@ -0,0 +1,3 @@
+//Think of something you could store in an array.
+let mountains: string[] = ["Everest" ," K2 " , "Anapurna" , "Nangaparbat"];
+console.log(" mountains i want to climb:", mountains );
